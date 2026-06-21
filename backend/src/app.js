1eed3d324b2db5990985613a0872dc2df00122bd
Priya Bhadane <bhadanepriya355@gmail.com>
2026-06-21 23:33:24 +0530

    Ready for deployment

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://bhadanepriya98_db_user:priya98@cluster0.3zp7yzi.mongodb.net/apnavideocall?appName=Cluster0");
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000");
+    });
+}
+
+start();
